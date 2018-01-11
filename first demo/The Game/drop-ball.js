8bd8a7ecace22ba1830a07490d8cc47df9cdfee9
Arwa Mohamed <arwaali467@gmail.com>
2018-01-11 12:07:50 -0800

    local storage

---
+++ first demo/The Game/drop-ball.js
@@ -0,0 +1,8 @@
+function drop ()
+{
+    var ball1 = document.createElement("img");
+    var src1 = document.createAttribute("src");
+    src1.value = "bal.ico";
+    var pos1 = document.createAttribute("class");
+    pos1.value="balls";
+}
---
+++ first demo/The Game/set-highest-score.js
@@ -0,0 +1,5 @@
+var get_Hscore=document.getElementById("score");
+if(localStorage.getItem("highest-score")>score);
+{
+    get_Hscore.textContent="Highest Score :"+localStorage.getItem("highest-score");
+}
