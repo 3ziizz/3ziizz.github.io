70a852f5daee98af7ac7d43a37cd976a586ac114
RaghdaHesham <raghdah.hesham@gmail.com>
2018-01-12 21:15:17 +0300

    audio for main and 3rd page done

--- first demo/The Game/move-box.js
+++ first demo/The Game/move-box.js
@@ -26,3 +26,21 @@ if(e.keyCode==39)
     }
 }
 }
+
+
+var aud = document.getElementById("myaudio1");
+
+var btn=document.getElementById("mybtn");
+btn.addEventListener('click',mute);
+
+function mute() {
+    document.getElementById("myaudio1").muted=!document.getElementById("myaudio1").muted ;
+}1
+
+function autoplay(){
+aud.autoplay = true;
+aud.loop=true ;
+aud.load();
+}
+
+autoplay();
--- first demo/js/page3.js
+++ first demo/js/page3.js
@@ -1,8 +1,16 @@
 
 var aud = document.getElementById("myaudio1");
+
+var btn=document.getElementById("mybtn");
+btn.addEventListener('click',mute);
+
+function mute() {
+    document.getElementById("myaudio1").muted=!document.getElementById("myaudio1").muted ;
+}1
+
 function autoplay(){
 aud.autoplay = true;
-aud.loop=true
+aud.loop=true ;
 aud.load();
 }
 
