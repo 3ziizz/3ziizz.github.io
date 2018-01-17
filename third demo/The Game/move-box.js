ed7f43ab02092c23a2e63a47f327f9dcd5ab93cb
Aziz <mohamed.abdelaziz1994@yahoo.com>
2018-01-17 17:55:30 +0200

    merge from raghda to master

---
+++ third demo/The Game/move-box.js
@@ -0,0 +1,56 @@
+var getbox=document.getElementById("box");
+document.documentElement.addEventListener("keydown",move);
+function move(e)
+{
+if(e.keyCode==39)
+{   
+    var arr =(getComputedStyle(getbox).right).split("p")
+    if(arr[0]<310)
+    {
+        getbox.style.right=(arr[0])+"px"
+    }else
+    {
+        getbox.style.right=(arr[0]-40)+"px"
+    }
+  
+}else if (e.keyCode==37)
+{
+    var arr =(getComputedStyle(getbox).right).split("p")
+    
+    if(arr[0]>815)
+    {
+        getbox.style.right=(arr[0])+"px"
+    }else
+    {
+        getbox.style.right=(arr[0]-0)+30+"px"
+    }
+}
+}
+
+
+var aud = document.getElementById("myaudio1");
+
+var btn=document.getElementById("mybtn");
+btn.addEventListener('click',mute);
+
+function mute() {
+    document.getElementById("myaudio1").muted=!document.getElementById("myaudio1").muted ;
+}
+
+function autoplay(){
+aud.autoplay = true;
+aud.loop=true ;
+aud.load();
+}
+
+autoplay();
+
+document.addEventListener("keyup",Gostart);
+function Gostart(e)
+{
+if (e.which==27)
+{
+document.location.href = '../start_page.html' ;
+}
+
+}
