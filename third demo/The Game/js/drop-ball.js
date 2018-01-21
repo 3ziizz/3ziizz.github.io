e1aa8c36230d718611c0066acef7f6eaa757da47
Aziz <mohamed.abdelaziz1994@yahoo.com>
2018-01-21 21:20:01 +0200

    adjust drop ball

--- third demo/The Game/js/drop-ball.js
+++ third demo/The Game/js/drop-ball.js
@@ -3,62 +3,20 @@ function randomIntFromInterval(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-/*function drop (image)
-{
-    ball1 = document.createElement("img");
-    var src1 = document.createAttribute("src");
-    src1.value = image;
-    ball1.setAttributeNode(src1);
-    var pos1 = document.createAttribute("class");
-    pos1.value="balls";
-    ball1.setAttributeNode(pos1);
-    ball1.style.right=randomIntFromInterval(280,1030)+"px";
-    document.body.appendChild(ball1);
-    move();
-    
-}
-function move()
-{   
-    var ball_arr=document.getElementsByClassName("balls");
-    for (var i=0;i<ball_arr.length;i++)   
-    {
-            var top_pos=(getComputedStyle(ball_arr[i]).top).split("p");
-            if(top_pos[0]<600)
-        {
-            ball_arr[i].style.top=(top_pos[0]-0)+10+"px";
-
-        }else
-        {
-            document.body.removeChild(ball_arr[i]);
-        }
-    }
-    
-
-}*/
-//setTimeout(drop("live.jpg"),1000);
-//setTimeout(drop,2000);
-//setTimeout(drop,4000);
-//var interval =setInterval(move,30); 
-
 var character=function(source)
 {
     this.source=source;
-
-}
-
-character.prototype.createball=function()
-{
-    ball1 = document.createElement("img");
+    ball = document.createElement("img");
     var src1 = document.createAttribute("src");
     src1.value = this.source;
-    ball1.setAttributeNode(src1);
+    ball.setAttributeNode(src1);
     var pos1 = document.createAttribute("class");
     pos1.value="balls";
-    ball1.setAttributeNode(pos1);
-    ball1.style.right=randomIntFromInterval(280,1030)+"px";
-    document.body.appendChild(ball1);
-    this.dropball();
-    
+    ball.setAttributeNode(pos1);
+    ball.style.right=randomIntFromInterval(280,1030)+"px";
+    console.log(typeof(ball));
+    document.body.appendChild(ball);
+
 }
 character.prototype.dropball=function()
 {   
@@ -68,15 +26,25 @@ character.prototype.dropball=function()
             var top_pos=(getComputedStyle(ball_arr[i]).top).split("p");
             if(top_pos[0]<600)
         {
-            ball_arr[i].style.top=(top_pos[0]-0)+10+"px";
+            ball_arr[i].style.top=(top_pos[0]-0)+20+"px";
 
         }else
         {
             document.body.removeChild(ball_arr[i]);
+            
         }
     }
     
 }
+
+var down =new character("img/Bal.ico");
+function cteate(){new character("img/Bal.ico")};
+setInterval(cteate,2000);
+setInterval(down.dropball,200);
+
+
+
+
 character.prototype.createheart=function()
 {
     heart1 = document.createElement("img");
@@ -109,10 +77,6 @@ character.prototype.dropheart=function()
     
 }
     
-    
-var ball2=new character("img/Bal.ico");
-ball2.createball();
-setInterval(ball2.dropball,30); 
 
 var heart=new character("img/prize.jpg");
 heart.createheart();
