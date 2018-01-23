2a8d37768d7658c0fbe25917a95dba43160c70e0
Aziz <mohamed.abdelaziz1994@yahoo.com>
2018-01-23 21:33:11 +0200

    add last one

---
+++ The Last one/js/navigate.js
@@ -0,0 +1,45 @@
+head=document.getElementById("start");
+main_game=document.getElementById("game");
+option_page=document.getElementById("option");
+exit_page=document.getElementById("exit");
+
+start_btn=document.getElementsByClassName("button1");
+option_btn=document.getElementsByClassName("button2");
+exit_btn=document.getElementsByClassName("button3");
+
+start_btn[0].addEventListener("click",game);
+option_btn[0].addEventListener("click",options)
+exit_btn[0].addEventListener("click",out);
+
+script1 = document.createElement("script");
+function game()
+{
+    head.style.display="none"
+    main_game.style.display="initial"
+    option_page.style.display="none"
+    exit_page.style.display="none"
+    //script1 = document.createElement("script");
+    var script1_src = document.createAttribute("src");
+    script1_src.value = "js/drop-ball.js";
+    script1.setAttributeNode(script1_src);
+    document.body.appendChild(script1);
+
+
+
+}
+function options()
+{
+    head.style.display="none"
+    main_game.style.display="none"
+    option_page.style.display="initial"
+    exit_page.style.display="none"
+
+}
+
+function out()
+{
+    head.style.display="none"
+    main_game.style.display="none"
+    option_page.style.display="none"
+    exit_page.style.display="initial"
+}
